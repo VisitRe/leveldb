8d64e17dfab93e6ba18421e5588919d9d678f37b
ivanabc <sjingivan@gmail.com>
2016-06-20 10:41:25 +0800

    reduce lock's range in DeleteObsoleteFiles
    * How to decide which range doesn't need lock
      1. Multiple thread write shared variable
      2. Multiple operations reply on one variable (read and set)
      3. Based on program logic, the info get within the lock won't be changed after the lock.

--- db/db_impl.cc
+++ db/db_impl.cc
@@ -215,11 +215,17 @@ void DBImpl::MaybeIgnoreError(Status* s) const {
 }
 
 void DBImpl::DeleteObsoleteFiles() {
+  mutex_.AssertHeld();
+
   if (!bg_error_.ok()) {
     // After a background error, we don't know whether a new version may
     // or may not have been committed, so we cannot safely garbage collect.
     return;
   }
+
+  uint64_t log_number = versions_->LogNumber();
+  uint64_t prev_log_number = versions_->PrevLogNumber();
+  uint64_t manifest_file_number = versions_->ManifestFileNumber();
 
   // Make a set of all of the live files
   std::set<uint64_t> live = pending_outputs_;
@@ -227,6 +233,8 @@ void DBImpl::DeleteObsoleteFiles() {
 
   std::vector<std::string> filenames;
   env_->GetChildren(dbname_, &filenames); // Ignoring errors on purpose
+
+  mutex_.Unlock();
   uint64_t number;
   FileType type;
   for (size_t i = 0; i < filenames.size(); i++) {
@@ -234,13 +242,13 @@ void DBImpl::DeleteObsoleteFiles() {
       bool keep = true;
       switch (type) {
         case kLogFile:
-          keep = ((number >= versions_->LogNumber()) ||
-                  (number == versions_->PrevLogNumber()));
+          keep = ((number >= log_number) ||
+                  (number == prev_log_number));
           break;
         case kDescriptorFile:
           // Keep my manifest file, and any newer incarnations'
           // (in case there is a race that allows other incarnations)
-          keep = (number >= versions_->ManifestFileNumber());
+          keep = (number >= manifest_file_number);
           break;
         case kTableFile:
           keep = (live.find(number) != live.end());
@@ -268,6 +276,7 @@ void DBImpl::DeleteObsoleteFiles() {
       }
     }
   }
+  mutex_.Lock();
 }
 
 Status DBImpl::Recover(VersionEdit* edit, bool *save_manifest) {
